167f0bd0d74018fc23911c023675aa2c117be991
Jean Fraga <jeanfragoli395@gmail.com>
2022-01-20 16:42:55 -0300

    fixed by prittier

---
+++ test/jest-setup.ts
@@ -0,0 +1,10 @@
+//Responsável por inicializar o server para todos os serves funcionarem;
+
+import { SetupServer } from '@src/server';
+import supertest from 'supertest';
+
+beforeAll(() => {
+  const server = new SetupServer();
+  server.init();
+  global.testRequest = supertest(server.getApp());
+});
