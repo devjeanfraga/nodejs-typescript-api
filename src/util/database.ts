4c399ecaebcae87639a880ade0c8cdd6c5cb33f4
Jean Fraga <jeanfragoli395@gmail.com>
2022-02-01 15:47:54 -0300

    add db connection e close connection

--- src/util/database.ts
+++ src/util/database.ts
@@ -1,2 +1,16 @@
 import config, {IConfig} from 'config';
-import mongoose, {Mongoose} from 'mongoose';
+import {connect as mongooseConnect ,connection} from 'mongoose';
+
+const dbConfig: IConfig = config.get('app.database');
+
+export const connect = async (): Promise<void> => {
+  await mongooseConnect(dbConfig.get('mongoURL'));
+};
+
+export const close = (): Promise<void> => connection.close();
+//Boa prática finalizar o banco de dados antes 
+// de finalizar a app pra garantir que nao crie nenhuma inconsistência
+
+
+
+
