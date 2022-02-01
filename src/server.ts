835985cb053d692603acbaa201f9ec1d32e654d8
Jean Fraga <jeanfragoli395@gmail.com>
2022-02-01 15:51:27 -0300

    impl the beachconntroller and method to close app

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import { Server } from '@overnightjs/core';
 import './util/module-alias';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
+import { BeachesController } from './controllers/beache';
 import { Application } from 'express';
+import * as database from '@src/util/database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -10,20 +12,31 @@ export class SetupServer extends Server {
   }
 
   //médoto para inicializar o servidor;
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.setupDatabase();
   }
 
   //cria o setup do Express ;
   private setupExpress(): void {
     this.app.use(bodyParser.json());
   }
-
+  //cria o setup do Controllers ;
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    this.addControllers([forecastController, beachesController]);
   }
+  //cria o setup do DB ;
+  private async setupDatabase(): Promise<void> {
+    await database.connect();
+  }
+
+//O close para fechar toda a nossa aplicação (boa prática);
+ public async  close(): Promise<void> {
+  await database.close(); 
+ } 
 
   public getApp(): Application {
     return this.app;
