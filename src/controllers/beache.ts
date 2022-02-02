cf3f8abd930058d5301438acccb88439d6e2518d
Jean Fraga <jeanfragoli395@gmail.com>
2022-02-01 22:03:06 -0300

    controller beaches

--- src/controllers/beache.ts
+++ src/controllers/beache.ts
@@ -1,12 +1,23 @@
 import { Controller, Post } from '@overnightjs/core';
-import {Request, Response} from 'express';
-
+import { Request, Response } from 'express';
+import { Beach } from '@src/models/beach';
+import mongoose from 'mongoose';
 
 @Controller('beaches')
 export class BeachesController {
   @Post('')
-  public async create( req: Request, res: Response): Promise<void> {
-    res.status(201).send({...req.body, id: 'fake-id'});
+  public async create(req: Request, res: Response): Promise<void> {
+    try {
+      const beach = new Beach(req.body);
+      const result = await beach.save();
+      res.status(201).send(result);
+    } catch (error) {
+      if( error instanceof mongoose.Error.ValidationError ) {
+        res.status(422).send({error: (error as Error).message});
+      } else {
+        res.status(500).send({error:  (error as Error).message});
+      }
+      
+    }
   }
-
 }
