49b1925d32ebf80969e6a6a68607e3f9d2fd33d9
Jean Fraga <jeanfragoli395@gmail.com>
2022-02-09 14:20:53 -0300

    create index to run server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('test');
+import { SetupServer } from './server';
+import config from 'config';
+
+// O init() do setupServer retorna uma promise entao devemos dar um await
+// o ideial seria usar o top-level-await mas esse projeto ainda não está configurado pra isso;
+// então vamos fazer a funçao se auto invocar;
+(async (): Promise<void> => {
+  const server = new SetupServer(config.get('app.port'));
+  await server.init();
+  server.start();
+})();
