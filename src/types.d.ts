8f75b06647cf62971568d78cba84bf2bb3adcf81
Jean Fraga <jeanfragoli395@gmail.com>
2022-02-13 19:08:36 -0300

    Override the namespace  global merge Request

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,8 +2,10 @@
 import * as http  from 'http';
 import { DecodedUser }  from '@src/services/auth';
 
-declare module 'express-server-static-core' {
+  declare module "express-serve-static-core"  {
   export interface Request extends http.IncomingMessage, Express.Request {
-    decoded?: DecodedUser;
+    decoded?:DecodedUser
   }
 }
+
+
