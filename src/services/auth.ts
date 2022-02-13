038336e289b887411eb1aecaf477ac5aa168ea7f
Jean Fraga <jeanfragoli395@gmail.com>
2022-02-13 09:54:26 -0300

    create method decodeToken()

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,11 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import{ User }from '@src/models/users'
 
+export interface DecodedUser extends Omit<User, '_id'> {
+  id: string; 
+}
 export default class AuthServices {
   public static async hashPassword (
     password: string, 
@@ -15,5 +21,16 @@ export default class AuthServices {
   ):Promise<boolean> {
 
   return await bcrypt.compare(pasword, hashedPassword);
-}
+  }
+
+  public static  generateToken(payload: object): string {
+    const token = jwt.sign(payload , config.get('app.auth.key') , {expiresIn: config.get('app.auth.tokenexpiresIn')});
+    return token; 
+  }
+
+  public static decodeToken(token: string): DecodedUser {
+   return  jwt.verify(token, config.get('app.auth.key')) as DecodedUser; 
+  
+  }
+
 }
