d1251654eccd43ebf3c06cc28dde95d869b9d207
Jean Fraga <jeanfragoli395@gmail.com>
2022-02-13 20:06:17 -0300

    insired the handle err to undefined token

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,10 +4,14 @@ import { NextFunction,Response, Request} from 'express';
 
 export function authMiddleware (
   req: Partial<Request>, // Com o partial vc torna opcional todas as propriedadesdo Request  tendo que usar o "?" nas propriedades;
-   _:Partial<Response>, // _ significa que nao será usado agora
+  res:Partial<Response>, // _ significa que nao será usado agora
   next: NextFunction):void {
-  const token = req.headers?.['x-access-token'];
-  const decode = AuthServices.decodeToken(token as string); //forçando o cast pra string;
-  req.decoded = decode;
-  next;
+    try {
+      const token = req.headers?.['x-access-token'];
+      const decode = AuthServices.decodeToken(token as string); //forçando o cast pra string;
+      req.decoded = decode;
+      next();
+    } catch (err) {
+      res.status?.(401).send({ code: 401, error: (err as Error).message})
+    }
 }
