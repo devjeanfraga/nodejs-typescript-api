948202fdcb8a5c82e3c0f512fa8f5eec605e72a9
Jean Fraga <jeanfragoli395@gmail.com>
2022-02-13 19:09:48 -0300

    create authenticate controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,8 @@ import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { BaseController } from '.';
 import { User } from '../models/users';
+import AuthServices from '@src/services/auth';
+
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -14,6 +16,27 @@ export class UsersController extends BaseController {
     } catch (error) {
        this.sendCreatedUpdateDataResponse(res, error);
        
+    }
+  }
+
+  @Post('authenticate')
+  public async authenticate(req: Request, res: Response): Promise<Response | undefined > {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if(!user) {
+      return res.status(401).send({
+        code: 401,
+        error: 'UNAUTHORIZED'
+      });
+    }
+    if(!(await AuthServices.comparePasword(password, user.password)) ) {
+      return res.status(401).send({
+        code: 401,
+        error: 'UNAUTHORIZED'
+      });
+    } else {
+      const token =  AuthServices.generateToken(user.toJSON());
+      return res.status(200).send({token: token});
     }
   }
 }
