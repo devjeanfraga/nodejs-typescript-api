5ef1a6a59e70c9d0a321991583a05f6197740529
Jean Fraga <jeanfragoli395@gmail.com>
2022-02-13 19:53:56 -0300

    insired test to handle undefined accessToken tkn

--- src/middlewares/__test__/auth.test.ts
+++ src/middlewares/__test__/auth.test.ts
@@ -15,5 +15,46 @@ describe('Auth Middleware', () => {
     expect(nextFake).toHaveBeenCalled();
   });
 
+  it('Should Return UNAUTHORIZED if there is a problem on the token verification', async () => {
+    const reqFake = {
+      headers: {
+        "x-access-token": 'invalid-Token'
+      }
+    };
 
+    const sendMock = jest.fn();
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock,
+      })), 
+    };
+
+    const nextFake = jest.fn();
+    authMiddleware(reqFake, resFake as object, nextFake);
+    expect(resFake.status).toHaveBeenCalledWith(401);
+    expect(sendMock).toHaveBeenCalledWith({
+      code: 401,
+      error: 'jwt malformed',
+    });
+  });
+
+  it('Should return UNAUTHORIZED middleware if there no token', async () => {
+    const reqFake = {
+      headers: {} // tratamento do return undefined
+    };
+
+    const sendMock= jest.fn();
+    const resFake = {
+      status: jest.fn(()=> ({
+        send: sendMock,
+      }))
+    };
+    const nextFake = jest.fn();
+    authMiddleware(reqFake, resFake as object, nextFake);
+    expect(resFake.status).toHaveBeenCalledWith(401);
+    expect(sendMock).toHaveBeenCalledWith({
+      code: 401,
+      error: 'jwt must be provided'
+    })
+  })
 })
