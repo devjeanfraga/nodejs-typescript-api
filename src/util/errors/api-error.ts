4a8892a8ab58ee968ae7804e809369ccd7322809
Jean Fraga <jeanfragoli395@gmail.com>
2022-02-17 20:25:59 -0300

    Create base API ERROR

---
+++ src/util/errors/api-error.ts
@@ -0,0 +1,35 @@
+import httpStatusCodes from 'http-status-codes';
+
+//  ***Description*** para que possa passar talvez como resolver o erro.
+//  Ou seguir alguma documentação para resolver o erro.
+//  ***Documentation*** para que possa ser apondtada a documentação do 
+//  erro explicando pq aquele erro acontece.
+export interface IAPIError {
+  message: string;
+  code: number;
+  codeAsString?: string; 
+  description?: string; 
+  documentation?: string; 
+}
+
+export interface APIErrorResponse extends Omit<IAPIError, 'codeAsString'> {
+  error: string;
+}
+
+export default class APIError {
+  public static format( error: IAPIError): APIErrorResponse {
+    return {
+      ...{
+        message: error.message,
+      code: error.code,
+      error: error.codeAsString 
+      ? error.codeAsString 
+      : httpStatusCodes.getStatusText(error.code)
+      },
+
+      //Coloca os campor description e ou documente somente se "settarmos" eles; 
+      ...(error.documentation && {documentation: error.documentation}),
+      ...(error.description && { description: error.description })
+    };
+  }
+}
