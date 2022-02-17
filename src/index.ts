aa1a5d0fa2ce40d69f000b841fa560cf92f5dc6c
Jean Fraga <jeanfragoli395@gmail.com>
2022-02-17 19:51:16 -0300

    add Gracefull shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { SetupServer } from './server';
 import config from 'config';
 import logger from './logger';
+import process from 'process';
 
 enum ExitStatus {
   Failure = 1,
@@ -8,6 +9,10 @@ enum ExitStatus {
 }
 
 
+//  Escuta todas as promises que receberam reject mas que nao tiveram Catch
+//  Jogamnos uma exceção que vai finalizar o processo e seguramente em seguida o 
+//  App reiniica( Tem custo baixo para ser reinicializada). Melhor que ficar
+//  enternamente. 
 process.on('unhandledRejection', (reason, promise) => {
   logger.error(
     `App exiting due to uncaught promise: ${promise} and ${reason}`
@@ -16,9 +21,9 @@ process.on('unhandledRejection', (reason, promise) => {
   throw reason;
 });
 
-process.on('unhandleException', (error)=>{
+process.on('uncaughtException', (error) => {
   logger.error(`App exiting due to an uncaught exception ${error}`)
-  throw process.exit(ExitStatus.Failure);
+  process.exit(ExitStatus.Failure);
 });
 // O init() do setupServer retorna uma promise entao devemos dar um await
 // o ideial seria usar o top-level-await mas esse projeto ainda não está configurado pra isso;
@@ -29,18 +34,19 @@ process.on('unhandleException', (error)=>{
     await server.init();
     server.start();
 
+    // *** Graceful shutdown ***
     const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
     exitSignals.map( (sig )=> {
-      process.on(sig, async () => {
+      process.on( sig , async () => {
         try {
           await server.close();
-          logger.info("App exit with success");
+          console.log("App exit with success");
           process.exit(ExitStatus.Success);
         } catch (error) {
-          logger.error(`App exit with error: ${error}`);
+          logger.error('App exit with error: ' + error);
           process.exit(ExitStatus.Failure);
         }
-      })
+      });
     });
   } catch (error) {
     logger.error(`App exit with error: ${error}`);
