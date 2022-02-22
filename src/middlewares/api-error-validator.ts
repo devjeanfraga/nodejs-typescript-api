fd5c7b14063076959d404f88209544ba0a880401
Jean Fraga <jeanfragoli395@gmail.com>
2022-02-22 19:07:00 -0300

    change send to json  response

--- src/middlewares/api-error-validator.ts
+++ src/middlewares/api-error-validator.ts
@@ -1,18 +1,18 @@
-import {Request, Response,  NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 import APIError from '@src/util/errors/api-error';
 
 export interface HTTPError extends Error {
   status?: number;
 }
 
-export function apiErrorValidator (
+export function apiErrorValidator(
   error: HTTPError,
   _: Partial<Request>,
   res: Response,
-  __: NextFunction 
+  __: NextFunction
 ): void {
   const errorCode = error.status || 500;
   res
     .status(errorCode)
-    .send(APIError.format({code: errorCode, message: error.message}));
+    .json(APIError.format({ code: errorCode, message: error.message }));
 }
